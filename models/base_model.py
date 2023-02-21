dd9020284f4df7e5b9eea6e7a2f6892353fe603b
Mikalach <mika.djetic@gmaiul.com>
2023-02-21 02:11:26 -0800

    pushing to the limit

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self, *args, **kwargs):
+    """def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -19,6 +19,25 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+"""
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
