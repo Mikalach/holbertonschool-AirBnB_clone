11d3d2887a41a48e8de875b865a51d02474deee6
Mikalach <mika.djetic@gmaiul.com>
2023-02-21 01:49:59 -0800

    last chance before suicide

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import json
-
-
+from os.path import isfile
+from models.base_model import BaseModel
 class FileStorage:
     __file_path = 'file.json'
     __objects = {}
@@ -19,10 +19,10 @@ class FileStorage:
             json.dump(d, f)
 
     def reload(self):
-        try:
-            with open(FileStorage.__file_path, mode='r', encoding='utf-8') as f:
-                d = json.load(f)
-            for key in d:
-                FileStorage.__objects[key] = FileStorage.__objects[d[key]["__class__"]](**d[key])
-        except FileNotFoundError:
-            pass
+        if not isfile(self.__file_path):
+            return
+        with open(FileStorage.__file_path, mode='r', encoding='utf-8') as f:
+            d = json.load(f)
+            for value in d.values():
+                self.new(eval(value["__class__"])(**value))
+        return
