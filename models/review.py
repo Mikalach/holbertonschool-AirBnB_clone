430381c6660628da4997177e68c71844434af0f7
Mikalach <mika.djetic@gmaiul.com>
2023-02-22 05:14:12 -0800

    review

--- models/review.py
+++ models/review.py
@@ -1,1 +1,17 @@
-review =
+#!/usr/bin/python3
+"""
+Review module
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Review class that inherits from BaseModel"""
+
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new Amenity."""
+        super().__init__(*args, **kwargs)
