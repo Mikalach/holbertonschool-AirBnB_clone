eb9892eb9860eba7505fb6ffc7c5958445f25340
Mikalach <mika.djetic@gmaiul.com>
2023-02-22 05:11:56 -0800

    city.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,1 +1,15 @@
-amenity = 
+#!/usr/bin/python3
+"""
+Amenity module
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class that inherits from BaseModel"""
+
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new Amenity."""
+        super().__init__(*args, **kwargs)
