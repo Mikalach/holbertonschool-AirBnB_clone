5b544ea379b05ae102a9e24a0d9894876260898a
Mikalach <mika.djetic@gmaiul.com>
2023-02-22 05:14:42 -0800

    state

--- models/state.py
+++ models/state.py
@@ -1,1 +1,15 @@
-state = 
+#!/usr/bin/python3
+"""
+State module
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State class that inherits from BaseModel"""
+
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new City."""
+        super().__init__(*args, **kwargs)
