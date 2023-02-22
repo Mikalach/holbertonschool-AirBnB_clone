0cafd2790295ceb9871911e90dbe203006672c50
Mikalach <mika.djetic@gmaiul.com>
2023-02-22 05:13:56 -0800

    city

--- models/city.py
+++ models/city.py
@@ -1,1 +1,16 @@
-city = 
+#!/usr/bin/python3
+"""
+City module
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """City class that inherits from BaseModel"""
+
+    state_id = ""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new City."""
+        super().__init__(*args, **kwargs)
