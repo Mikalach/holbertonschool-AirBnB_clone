8e49bd27d99558ed3350a9d78c32fabbfbd83392
Mikalach <mika.djetic@gmaiul.com>
2023-02-23 02:05:20 -0800

    peped

--- models/base_model.py
+++ models/base_model.py
@@ -6,20 +6,7 @@ from datetime import datetime
 
 
 class BaseModel:
-    """def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != '__class__':
-                    setattr(self, key, value)
-            self.created_at = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-"""
+    """ Base modeled """
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel.
         Args:
@@ -40,7 +27,8 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(type(self).__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
