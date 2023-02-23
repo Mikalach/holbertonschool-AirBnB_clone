736234c3b11a905b3dc1e3040ca27540f96ebba1
Mikalach <mika.djetic@gmaiul.com>
2023-02-23 02:07:55 -0800

    peped2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,10 +8,11 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+
+
 class FileStorage:
     __file_path = 'file.json'
     __objects = {}
-
 
     def all(self):
         return FileStorage.__objects
